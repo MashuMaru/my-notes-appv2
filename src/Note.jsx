d79eb5434022f4dcfcdf207c4825739bdd63baab
MashuMaru <52470960+MashuMaru@users.noreply.github.com>
2020-10-15 06:56:19 +0100

    added delete button

--- src/Note.jsx
+++ src/Note.jsx
@@ -8,11 +8,16 @@ function clickedContents () {
     alert("Add the contents of the note.")
 }
 
+function deleteNote() {
+    alert("Delete this note.")
+}
+
 function Note() {
     return <div className="note">
     {/* <div className="note-move">1</div> */}
     <input className="note-title" placeholder="title..." onClick={clickedTitle}></input>
     <textarea cols="30" rows="5" className="note-contents" placeholder="contents..." onClick={clickedContents}></textarea>
+    <img src="/recycle-bin.png" alt="delete" onClick={deleteNote}></img>
     </div>
 }
 
