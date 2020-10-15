9a93ec41ffa843c8fb003b79c2bf9ded2fe8be72
MashuMaru <52470960+MashuMaru@users.noreply.github.com>
2020-10-15 06:38:08 +0100

    added 'plus' to header with check JS.

--- src/Header.jsx
+++ src/Header.jsx
@@ -1,8 +1,13 @@
 import React from "react";
 
+function addNote(){
+    alert("add a new note.")
+}
+
 function Header() {
     return <div className="header">
         <h1>My Sticky Notes App</h1>
+        <img src="plus.png" alt="plus" onClick={addNote}></img>
     </div>
     }
 
