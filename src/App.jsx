188a186a3d1d69b6ca3c9779480015de389f403e
MashuMaru <52470960+MashuMaru@users.noreply.github.com>
2020-10-16 06:33:06 +0100

    added 'add' button to main App.jsx for appending <Note />..

--- src/App.jsx
+++ src/App.jsx
@@ -3,15 +3,20 @@ import Header from "./Header.jsx";
 import Footer from "./Footer.jsx";
 import Note from "./Note.jsx";
 
+function addNote() {
+    alert("Add note.")
+}
 
 function App() {
     return <div>
 <Header />
+<img className="add-img" src={require('./images/plus.png')} alt="add" onClick={addNote}></img>
 <div className="grid-container">
 <Note />
 <Note />
 <Note />
-
+<Note />
+<Note />
 </div>
 <Footer />
     </div>
