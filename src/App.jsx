c073e2ce9b9f0e8b7e41eaec6aa520ab86bc79c7
MashuMaru <52470960+MashuMaru@users.noreply.github.com>
2020-11-09 05:09:41 +0000

    isExpanded added - working on onDelete. (not working)

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,32 @@
-import React from "react";
+import React, { useState } from "react";
 import Header from "./Header.jsx";
 import Footer from "./Footer.jsx";
 import Note from "./Note.jsx";
 
 function App() {
+    const [notes, setNotes] = useState([]);
+
+    function deleteNote(id) {
+        setNotes(prevNotes => {
+            return prevNotes.filter((noteItem, index) => {
+                return index !== id;
+            })
+        })
+    }
+
     return <div>
             <Header />
             <section className="body-section">
-                <Note />
+            <Note />
+            {notes.map((noteItem, index) => {
+                return (<Note 
+                key={index}
+                id={index}
+                onDelete={deleteNote}
+                />);
+            })}
+                
+
 
             </section>
             <Footer />
--- src/Note.jsx
+++ src/Note.jsx
@@ -2,23 +2,22 @@ import React, { useState } from "react";
 
 
 
-function Note() {
+function Note(props) {
 
     const [isExpanded, setExpanded] = useState(false);
 
-    function deleteNote() {
-        alert("Delete note.");
+    function handleClick(props) {
+        props.onDelete(props.id);
     }
     
     function expand() {
         setExpanded(true);
-        
     }
 
     return <div className="note">
     <input className="note-title" placeholder="title..." onClick={expand}></input>
     {isExpanded ? (<textarea cols="30" rows="5" className="note-contents" placeholder="contents..."></textarea>) : null}
-    {isExpanded ? (<img className="delete-img" src={require('./images/recycle-bin.png')} onClick={deleteNote} alt="delete"></img>) : null}
+    {isExpanded ? (<img className="delete-img" src={require('./images/recycle-bin.png')} onClick={handleClick} alt="delete"></img>) : null}
     </div>
 }
 
