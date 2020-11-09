7502f9a364e01aa22e4c57853375a38cf50746df
MashuMaru <52470960+MashuMaru@users.noreply.github.com>
2020-11-09 06:32:15 +0000

    added JS to add button (onMouseOver & onMouseOut, show <p>add note</p>

--- src/Header.jsx
+++ src/Header.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, {useState} from "react";
 
 
 function addNote(){
@@ -6,10 +6,25 @@ function addNote(){
 }
 
 function Header() {
+    const [isMouseOver, setMouseOver] = useState(false);
+    const [isMouseOut, setMouseOut] = useState(true);
+
+    function handleMouseOver() {
+        setMouseOver(true);
+    }
+
+    function handleMouseOut() {
+        setMouseOut(setMouseOver(false));
+    }
+
     return <div className="header">
-        <h1 className="header-text">My Sticky Notes App</h1>
-        <img className="add-img" src={require('./images/plus.png')} alt="add" onClick={addNote}></img>
-    </div>
+                <h1 className="header-text">My Sticky Notes App</h1>
+                    <div className="add-button" onMouseOver={handleMouseOver}>
+                        <img className="add-img" src={require('./images/plus.png')} alt="add" onClick={addNote} onMouseOut={handleMouseOut}></img>
+                    {isMouseOver ? <p className="add-p">add note</p> : null}
+                    {isMouseOut && null }
+                    </div>
+            </div>
     }
 
 
