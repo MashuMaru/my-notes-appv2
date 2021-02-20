65ab9106686097c7f896c3956fec88f6078a8cca
MashuMaru <52470960+MashuMaru@users.noreply.github.com>
2021-02-20 15:56:08 +0000

    add new note function, and remove note function

--- src/App.jsx
+++ src/App.jsx
@@ -7,24 +7,52 @@ class App extends Component {
 
     state = {
         notes: [
-           { id: 0}
+           { 
+               id: 0
+            }
         ]
     }
+
+    prevNoteID = 1;
+
+    handleAddNote = (id) => {
+        this.setState(prevState => {
+            return {
+                notes: [
+                    ...prevState.notes,
+                    {
+                        id: this.prevNoteID += 1
+                    }
+                ]
+            }
+        })
+    }
+
+    handleRemoveNote = (id) => {
+        this.setState (prevState => {
+            return {
+                notes: prevState.notes.filter(n => n.id !== id)
+            }
+        });
+    }
+
     render () {
 
     return (
             <div>
-                <Header />
-                {/* <section id="note-section" className="body-section"> */}
-                <Note />
-                <Note />
-                <Note />
-                <Note />
-                <Note />
-                <Note />
-                <Note />
-                <Note />
-                {/* </section> */}
+                <Header 
+                    addNote={this.handleAddNote}
+                />
+                <section id="note-section" className="body-section">
+                {this.state.notes.map( (note, index) => 
+                <Note 
+                    id={note.id}
+                    key={note.id.toString()}
+                    index={index}
+                    removeNote={this.handleRemoveNote} 
+                />)} 
+
+                </section> 
                 <Footer />
             </div>)
             }
--- src/Header.jsx
+++ src/Header.jsx
@@ -23,7 +23,7 @@ class Header extends Component {
         return <div className="header">
         <h1 className="header-text">My Sticky Notes App</h1>
             <div className="add-button">
-                <img className="add-img" src={require('./images/plus.png')} alt="add" onMouseOver={this.handleMouseOver} onMouseOut={this.handleMouseOut}></img>
+                <img className="add-img" src={require('./images/plus.png')} alt="add" onClick={() => this.props.addNote()}onMouseOver={this.handleMouseOver} onMouseOut={this.handleMouseOut}></img>
                 {this.state.isMousedOver ? <p className="add-p">add note...</p> : null}
                 {this.state.isMousedOut && null }
             </div>
