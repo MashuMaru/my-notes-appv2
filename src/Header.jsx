6afb7bcb7d54a5b3c805d01c4401cd5952d89360
MashuMaru <52470960+MashuMaru@users.noreply.github.com>
2021-02-20 08:54:29 +0000

    class component for Header

--- src/Header.jsx
+++ src/Header.jsx
@@ -1,31 +1,71 @@
-import React, {useState} from "react";
+import React, {Component} from "react";
 
-function Header(props) {
-    const [isMouseOver, setMouseOver] = useState(false);
-    const [isMouseOut, setMouseOut] = useState(true);
+class Header extends Component {
+    
+    state = {
+        isMousedOver: false,
+    }
+    
 
-    function addNote() {
-        props.onAdd();
+    handleMouseOver = () => {
+        this.setState({
+            isMousedOver: true
+        })
     }
 
-    function handleMouseOver() {
-        setMouseOver(true);
+    handleMouseOut = () => {
+        this.setState( {
+            isMousedOver: false
+        })
     }
 
-    function handleMouseOut() {
-        setMouseOut(setMouseOver(false));
-    }
-
-    return <div className="header">
-                <h1 className="header-text">My Sticky Notes App</h1>
-                    <div className="add-button">
-                        <img className="add-img" src={require('./images/plus.png')} alt="add" onClick={addNote} onMouseOver={handleMouseOver} onMouseOut={handleMouseOut}></img>
-                        {isMouseOver ? <p className="add-p">add note...</p> : null}
-                        {isMouseOut && null }
-                    </div>
+    render() {
+        return <div className="header">
+        <h1 className="header-text">My Sticky Notes App</h1>
+            <div className="add-button">
+                <img className="add-img" src={require('./images/plus.png')} alt="add" onMouseOver={this.handleMouseOver} onMouseOut={this.handleMouseOut}></img>
+                {this.state.isMousedOver ? <p className="add-p">add note...</p> : null}
+                {this.state.isMousedOut && null }
             </div>
+    </div>
+}
+
     }
+   
 
 
 
 export default Header;
+
+
+// import React, {useState} from "react";
+
+// function Header(props) {
+//     const [isMouseOver, setMouseOver] = useState(false);
+//     const [isMouseOut, setMouseOut] = useState(true);
+
+//     function addNote() {
+//         props.onAdd();
+//     }
+
+//     function handleMouseOver() {
+//         setMouseOver(true);
+//     }
+
+//     function handleMouseOut() {
+//         setMouseOut(setMouseOver(false));
+//     }
+
+//     return <div className="header">
+//                 <h1 className="header-text">My Sticky Notes App</h1>
+//                     <div className="add-button">
+//                         <img className="add-img" src={require('./images/plus.png')} alt="add" onClick={addNote} onMouseOver={handleMouseOver} onMouseOut={handleMouseOut}></img>
+//                         {isMouseOver ? <p className="add-p">add note...</p> : null}
+//                         {isMouseOut && null }
+//                     </div>
+//             </div>
+//     }
+
+
+
+// export default Header;
--- src/Note.jsx
+++ src/Note.jsx
@@ -1,7 +1,5 @@
 import React, { Component } from "react";
 
-
-
 class Note extends Component {
 
     state = {
@@ -21,33 +19,7 @@ class Note extends Component {
                 {this.state.isExpanded ? (<textarea cols="30" rows="5" className="note-contents" placeholder="contents..."></textarea> ) : null}
                 {this.state.isExpanded ? (<img className="delete-img" src={require('./images/recycle-bin.png')} alt="delete"></img>) : null}
             </div>)
-            }
+        }
     }
-export default Note;
-
-// import React, { useState } from "react";
-
-
-
-// function Note(props) {
-
-//     const [isExpanded, setExpanded] = useState(false);
-
-//     function handleClick() {
-//         props.onDelete();
-//     }
     
-//     function expand() {
-//         setExpanded(true);
-//     }
-
-//     return <div className="note" value={props.noteItem}>
-//     <input className="note-title" placeholder="title..." onClick={expand}></input>
-//     {isExpanded ? (<textarea cols="30" rows="5" className="note-contents" placeholder="contents..."></textarea> ) : null}
-//     {isExpanded ? (<img className="delete-img" src={require('./images/recycle-bin.png')} onClick={handleClick} alt="delete"></img>) : null}
-//     {/* <img className="delete-img" src={require('./images/recycle-bin.png')} onClick={handleClick} alt="delete"></img> */}
-
-//     </div>
-// }
-
-// export default Note;
+export default Note;
