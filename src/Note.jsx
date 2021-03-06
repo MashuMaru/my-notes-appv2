87c13dad16a1cbc7482748b64f0cc63817ce3328
MashuMaru <52470960+MashuMaru@users.noreply.github.com>
2021-03-06 20:26:44 +0000

    amendment to localStorage

--- src/Note.jsx
+++ src/Note.jsx
@@ -4,7 +4,51 @@ class Note extends Component {
   state = {
     isExpanded: false,
     isMousedOver: false,
+    isMousedOverSave: false,
+    descInput: "",
   };
+
+  userData;
+
+  constructor(props) {
+    super(props);
+
+    this.onChangeTitle = this.onChangeTitle.bind(this);
+    this.onChangeDesc = this.onChangeDesc.bind(this);
+
+    this.state = {
+      titleInput: "",
+      descInput: "",
+    };
+  }
+
+  onChangeTitle = (e) => {
+    this.setState({
+      titleInput: e.target.value,
+    });
+  };
+
+  onChangeDesc = (e) => {
+    this.setState({
+      descInput: e.target.value,
+    });
+  };
+
+  componentDidMount() {
+    this.userData = JSON.parse(localStorage.getItem("note"));
+
+    if (localStorage.getItem("note")) {
+      this.setState({
+        titleInput: this.userData.titleInput,
+        descInput: this.userData.descInput,
+      });
+    } else {
+      this.setState({
+        titleInput: "",
+        descInput: "",
+      });
+    }
+  }
 
   handleMousedOver = () => {
     this.setState({
@@ -17,6 +61,7 @@ class Note extends Component {
     });
   };
 
+
   showNote = () => {
     this.setState({
       isExpanded: true,
@@ -26,21 +71,26 @@ class Note extends Component {
   render() {
     return (
       <div className="note">
-        <input
-          onClick={() => this.showNote()}
-          className="note-title"
-          placeholder="title..."
-        ></input>
+        <form>
+          <input
+            onClick={() => this.showNote()}
+            className="note-title"
+            placeholder="title..."
+            value={this.state.titleInput}
+            onChange={this.onChangeTitle}
+          ></input>
 
-        {this.state.isExpanded ? (
-          <textarea
-            cols="30"
-            rows="5"
-            className="note-contents"
-            placeholder="contents..."
-          ></textarea>
-        ) : null}
-
+          {this.state.isExpanded ? (
+            <textarea
+              cols="30"
+              rows="5"
+              className="note-contents"
+              placeholder="contents..."
+              value={this.state.descInput}
+              onChange={this.onChangeDesc}
+            ></textarea>
+          ) : null}
+        </form>
         <img
           onClick={() => this.props.removeNote(this.props.id)}
           className="delete-img"
